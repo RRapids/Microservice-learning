3c138c52740544e61c3db687f5a110674bae3820
RRapids <tengfeiqaq@gmail.com>
2020-09-20 15:28:42 +0800

    微服务

---
+++ course-center/src/main/java/com/soft1851/coursecenter/dao/CourseMapper.java
@@ -0,0 +1,10 @@
+package com.soft1851.coursecenter.dao;
+
+import com.soft1851.coursecenter.domain.entity.Course;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author tengf
+ */
+public interface CourseMapper extends Mapper<Course> {
+}
